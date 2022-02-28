94fe9effbcfc6d74e9b0562d39d48f37e8f3d410
MyNameIsKitsune <i.misterok21@gmail.com>
2022-02-28 16:11:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pacman -Syu
+pacman -Syu --noconfirm
 pacman -S --noconfirm tor proxychains wget
 wget -O /usr/bin/bombardier "https://github.com/codesenberg/bombardier/releases/download/v1.2.5/bombardier-linux-amd64"
 chmod +x /usr/bin/bombardier
