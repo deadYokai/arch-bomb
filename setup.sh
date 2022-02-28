f84d1935bbc1066b92b4a590d57b3c702734d04f
MyNameIsKitsune <i.misterok21@gmail.com>
2022-02-28 17:02:47 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pacman -Syu --noconfirm
-pacman -S --noconfirm tor proxychains wget
-wget -O /usr/bin/bombardier "https://github.com/codesenberg/bombardier/releases/download/v1.2.5/bombardier-linux-amd64"
-chmod +x /usr/bin/bombardier
-
