403e2361e91b2149e1d93f3626ad452ad7d83dcb
MyNameIsKitsune <i.misterok21@gmail.com>
2022-03-02 14:55:01 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,4 @@
 @echo off
+wsl --set-version Arch 2
 bash -c "pacman-key --init && pacman-key --populate archlinux && pacman -Syu --noconfirm && pacman -S --noconfirm tor proxychains wget && wget -O /usr/bin/bombardier https://github.com/codesenberg/bombardier/releases/download/v1.2.5/bombardier-linux-amd64 && chmod +x /usr/bin/bombardier"
 pause
